e4b0aa64e85305386e3d76a8c256cb82b552db42
RyanRENJITH25 <86664584+RyanRENJITH25@users.noreply.github.com>
2021-08-13 23:11:08 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var balloon,balloonImage1,balloonImage2;
+ var database;
+  var position;
+
+
+  function preload(){ 
+    bg =loadImage("cityImage.png");
+     balloonImage1=loadAnimation("hotairballoon1.png");
+      balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png", "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png", "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+     }
+     
+     
+     function setup() { 
+    createCanvas(1500,700);
+    balloon=createSprite(250,450,150,150);
+    balloon.addAnimation("hotAirBalloon",balloonImage1); 
+    balloon.scale=0.5; 
+    textSize(20); 
+  }  
+  
+  { background(bg); if(keyDown(LEFT_ARROW))
+    
+    { balloon.x = balloon.x - 2 balloon.addAnimation("hotAirBalloon",balloonImage2);
+   } else if(keyDown(RIGHT_ARROW)){ balloon.x = balloon.x + 2 balloon.addAnimation("hotAirBalloon",balloonImage2);
+   } else if(keyDown(UP_ARROW)){ balloon.y = balloon.y - 2 balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.001;
+   } else if(keyDown(DOWN_ARROW)){ balloon.y = balloon.y + 2 balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale +0.001;
+   } function updateHeight(x,y){ database.ref('balloon/height').set({ 'x': height.x + x, 'y': height.y + y }) } function readHeight(data){ height = data.val(); 
+    balloon.x = height.x;
+     balloon.y = height.y;
+     } function showError(){ console.log("Error in writing to the database") } drawSprites();
+      fill(0);
+       stroke("white");
+        textSize(25);
+         text("**Use arrow keys to move Hot Air Balloon!",40,40);
+         }
